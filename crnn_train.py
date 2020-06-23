897652a7c2aa27dbb8e8d432c64a79847b0239c2
mingweichina <mingweichina@126.com>
2020-06-23 18:33:33 +0800

    update

--- crnn_train.py
+++ crnn_train.py
@@ -10,34 +10,33 @@ warnings.filterwarnings("ignore")
 
     
 if __name__ == '__main__':
-
-    # 0 Prepare test_data for validation 
-    test_names = os.listdir(data_loader.TEST_LIST[0])
-    test_iter = iter(DataGenerator(data_loader.TEST_LIST,5000))
-    test_data = next(test_iter)
     
-    
-    # 1 Load pre-train weight
+    # 0 Load pre-train weight
     from crnn_tools.core import CRNN
     train_model = CRNN()   
-    train_model._load_weights("weights/rd_2.h5")
+    train_model._load_weights("weights/rd_5.h5")
     
     
     # 2 Begin training
     for i in range(10):
         dll = DataGenerator(data_loader.TRAIN_LIST,5000) 
         it_dll = iter(dll)
-        j = i+3
+        j = i+6
         lr = (1e-3)*(0.995)**j
         sgd = SGD(lr=lr)
         train_model._compile_net(sgd)
         
+        saved_count = 0
         for epoch_iter in range(dll.__len__()):
             print(epoch_iter/dll.__len__())
             tem_data = next(it_dll)
             his = train_model.train_core.fit(x=tem_data[0],y=tem_data[1],epochs=1,callbacks = [tf.keras.callbacks.History()],batch_size=16)
             del(tem_data)
-            
+            saved_count+=1
+            if saved_count==50:
+                 train_model.train_core.save_weights("weights/rd_"+str(j)+".h5")
+                 saved_count = 0
+    
         train_model.train_core.save_weights("weights/rd_"+str(j)+".h5")
             
         
--- crnn_validation.py
+++ crnn_validation.py
@@ -1,55 +1,42 @@
-import tensorflow as tf
 import os
 import numpy as np
-from tensorflow.keras.optimizers import SGD
+from crnn_tools.data_loader import libs
+from crnn_tools import data_loader
+from crnn_tools.core import load_picture
 import warnings
+import difflib
+from tqdm import tqdm
 warnings.filterwarnings("ignore")
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+ 
+def string_similar(s1, s2):
+    return difflib.SequenceMatcher(None, s1, s2).quick_ratio()
 
     
 if __name__ == '__main__':
 
-    # 0 Prepare test_data for validation 
+    # 0 Prepare the name of test images
     test_names = os.listdir(data_loader.TEST_LIST[0])
-    test_iter = iter(DataGenerator(data_loader.TEST_LIST,5000))
-    test_data = next(test_iter)
-    
+
     
     # 1 Load pre-train weight
     from crnn_tools.core import CRNN
+    valid_model = CRNN()   
+    valid_model._load_weights("weights/rd_5.h5")
     
     
-    train_model = CRNN()   
-    train_model._load_weights("weights/rd_5.h5")
-    
-    
-    path = "asset/english.jpg"
-    img_gray = load_picture(path)
-    train_model.predict_and_translate(img_gray)
-    
-    
-    
-    img_gray = load_picture(path)
-     
-    train_model.predict_core.predict(img_gray)
-    
-    
-    # 2 Begin training
-    for i in range(10):
-        dll = DataGenerator(data_loader.TRAIN_LIST,5000) 
-        it_dll = iter(dll)
-        j = i+3
-        lr = (1e-3)*(0.995)**j
-        sgd = SGD(lr=lr)
-        train_model._compile_net(sgd)
-        
-        for epoch_iter in range(dll.__len__()):
-            print(epoch_iter/dll.__len__())
-            tem_data = next(it_dll)
-            his = train_model.train_core.fit(x=tem_data[0],y=tem_data[1],epochs=1,callbacks = [tf.keras.callbacks.History()],batch_size=16)
-            del(tem_data)
-            
-        train_model.train_core.save_weights("weights/rd_"+str(j)+".h5")
-            
-        
-        
+    # 2 Begin evaluating
+    result = []
+    try:
+        with tqdm(test_names) as t:
+            for name in t:
+                path_name = os.path.join(data_loader.TEST_LIST[0],name)
+                img_gray = load_picture(path_name)
+                pre_str = valid_model.predict_and_translate(img_gray)
+                true_str = ''.join(libs.total_list[libs.total_dic[name]])
+                result.append(string_similar(pre_str[0],true_str))
+    except:
+        t.close()
+    print(np.mean(result))
 
